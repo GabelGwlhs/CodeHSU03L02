f109b70054f69a97c07852a7ed56af9bc629e70d
GabelGwlhs <113708877+GabelGwlhs@users.noreply.github.com>
2022-11-02 09:31:20 -0700

    completed tasks

--- BooleanFun.java
+++ BooleanFun.java
@@ -1,15 +1,37 @@
+
+import java.util.Scanner;
 public class BooleanFun {
     public static void main(String[] args) {
-        int myAge = 35;
-
+        Scanner input = new Scanner(System.in);
+        int myAge = input.nextInt();
         System.out.println("Current age: " + myAge);
 
         //convert these into something more complete using if statements
-        System.out.println("Can rent a car: " + (myAge >= 25));
-        System.out.println("Gets a senior discount: "+ (myAge >= 65));
-        System.out.println("Can order from children's menu: " + (myAge <=12));
 
-        System.out.println("On your next birthday out you will be: "); //complete this code, don't do math in sout
+        if (myAge >= 25) {
+            System.out.println("You can rent a car");
+
+        }
+        if(myAge < 25){
+            System.out.println("You cannot rent a car :(");
+
+        }
+
+        if (myAge>=65){
+            System.out.println("You can have a senior discount");
+        }
+        else {
+            System.out.println("You cannot have a senior discount");
+        }
+
+        if (myAge<=12){
+            System.out.println("You can order off the childrens menu");
+        }
+        else{
+            System.out.println("You cannot order off the childrens menu");
+        }
+        int nextAge = myAge+1;
+        System.out.println("On your next birthday out you will be: "+nextAge); //complete this code, don't do math in sout
 
         //further modification: prompt user for input and then print what they are able to do based on their age
         //and print their age on their next birthday
